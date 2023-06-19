7c93a76a572869a4e76c6a6fa93aee835b811c76
Ata Safapour <safapour.ata@gmail.com>
2023-06-19 21:05:27 +0330

    build: django user model used for accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,22 +1,38 @@
 from django.db import models
-from core.models import BaseModel
+from django.contrib.auth.models import User
+# from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
 
 
 
-class Account(BaseModel):
+class Account(models.Model):
 
-    name = models.CharField(_("Name"), max_length=100)
+    user = models.ForeignKey("User", verbose_name=_("User"), on_delete=models.CASCADE, related_name='account')
+    # name = models.CharField(_("Name"), max_length=100)
     avatar = models.ImageField(_("Avatar"), upload_to='uploads/avatars/', height_field=None, width_field=None, max_length=None) #alt = name
-    bio = models.TextField(_("Bio"))
-    password = models.CharField(_("Password hass"), max_length=250)
-    email = models.EmailField(_("Email"), max_length=254)
+    bio = models.TextField(_("Bio"), null=True, blank=True)
+    # password = models.CharField(_("Password hass"), max_length=250)
+    # email = models.EmailField(_("Email"), max_length=254)
     birthdate = models.DateField(_("Birthdate"), auto_now=False, auto_now_add=False)
 
     @property
     def user_posts(self):
         ...
     
+    def create_account():
+        ...
+
+    def follow(self, other):
+        relation
+
+    def archive(self):
+        self.is_active = False
+        self.save()
+
+    def restore(self):
+        self.is_active= True
+        self.save()
+
     class Meta:
         verbose_name = _("User")
         verbose_name_plural = _("Users")
