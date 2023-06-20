552c2c75876619f6cdbd5cedba74721beaa400dd
Ata Safapour <safapour.ata@gmail.com>
2023-06-20 14:46:05 +0330

    feat: added methods to account

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
-
+from relations.models import UserFollow
 
 
 class Account(models.Model):
@@ -17,13 +17,25 @@ class Account(models.Model):
 
     @property
     def user_posts(self):
-        ...
+        return self.posts.all()
     
-    def create_account():
-        ...
+    @classmethod
+    def create_account(cls, username, password, avatar, bio, birthdate, **kwargs):
+        user = User.objects.create(username= username, password= password, extra_fields=kwargs)
+        return cls.objects.create(user= user, avatar= avatar, bio= bio, birthdate= birthdate)
+
+    def user_followers(self):
+        return self.followers.all()
+    
+    def user_followees(self):
+        return self.followees.all()
+
+    def is_followed(self, other):
+        return UserFollow.objects.filter(follower= self, followee= other).exists()
 
     def follow(self, other):
-        ...
+        if not self.is_followed(self, other):
+            UserFollow.objects.create(follower= self, followee= other)
 
     def archive(self):
         self.is_active = False
@@ -38,7 +50,7 @@ class Account(models.Model):
         verbose_name_plural = _("Accounts")
 
     def __str__(self):
-        return f'{self.bio}'
+        return f'{self.user.username}'
 
 
 
