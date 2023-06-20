13de445d3f9e02f144847c1738e46f59c9298276
Ata Safapour <safapour.ata@gmail.com>
2023-06-20 04:10:11 +0330

    build: profile view added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 
+from .models import Account
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+def profile_view(request, username):
+    user = User.objects.get(username=username)
+    print(user)
+    account = get_object_or_404(Account, user=user)
+    context = {'account': account, "user": user}
+    return render(request, 'accounts/account.html', context)
