fd6c7e4a3e602fab529bb7ed25da05689f2ba657
Ata Safapour <safapour.ata@gmail.com>
2023-06-25 06:15:48 +0330

    build: PostListView added

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from django.forms import forms
+from .models import Post
 
-# Create your views here.
+
+
+class PostListView(View):
+
+    def get(self, request):
+        posts = Post.objects.all().order_by('-create_at')
+        context = {'posts': posts}
+        return render(request, 'posts/post_list.html', context)
+
+
+
+
