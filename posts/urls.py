2a006089b1846050cbd810b924167a2df2295ef1
Ata Safapour <safapour.ata@gmail.com>
2023-07-04 09:04:28 +0330

    build: create post url, veiw added

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostLikeView, PostCommentView
+from .views import PostListView, PostLikeView, PostCommentView, PostCreateView
 
 app_name = 'posts'
 urlpatterns = [
     path("all/", PostListView.as_view(), name="post_list"),
     path('like/<int:post_id>', PostLikeView.as_view(), name='like'), 
-    path("comments/<int:post_id>/<str:on>", PostCommentView.as_view(), name="comment")
+    path("comments/<int:post_id>/<str:on>", PostCommentView.as_view(), name="comment"),
+    path("new-post/", PostCreateView.as_view(), name="new_post")
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from django.forms import forms
-from .models import Post, Comment
+from .forms import PostForm, MediaForm, HashtagForm
+from .models import Post, Comment, Media, Hashtag
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class PostListView(View):
 
@@ -12,6 +14,39 @@ class PostListView(View):
         context = {'posts': posts}
         return render(request, 'posts/post_list.html', context)
 
+class PostCreateView(View):
+    template = 'posts/post_create.html'
+
+    def get(self, request):
+        form = PostForm(initial={'user_account': request.user.account.get()})
+        media_form = MediaForm
+        hashtag_form = HashtagForm
+        return render(request, self.template, {'form': form, 'media_form': media_form, 'hashtag_form': hashtag_form})
+
+    @method_decorator(login_required)
+    def post(self, request):
+        form = PostForm(request.POST)
+        hashtag_form = HashtagForm(request.POST)
+        media_form = MediaForm(request.POST, request.FILES)
+        if form.is_valid() and hashtag_form.is_valid() and media_form.is_valid():
+            cd = form.cleaned_data
+            tag = hashtag_form.cleaned_data['tag']
+            if tag:
+                if not Hashtag.objects.filter(tag = tag).exists():
+                    hashtag = Hashtag.objects.create(tag= tag)
+                else:
+                    hashtag = Hashtag.objects.get(tag = tag)
+                post = Post.objects.create(user_account= cd['user_account'], description =cd['description'])
+                hashtag.user_post.add(post)
+            else:
+                post = Post.objects.create(user_account= cd['user_account'], description =cd['description'])
+
+            media_cd = media_form.cleaned_data
+            Media.objects.create(user_media= media_cd['user_media'], is_default=media_cd['is_default'], user_post= post)
+
+            return redirect('accounts:profile', cd['user_account'].user.username)
+        return redirect('posts:new_post')
+        
 
 class PostLikeView(LoginRequiredMixin ,View):
 
