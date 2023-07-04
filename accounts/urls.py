8a722b7b1e0b842dc7d1e4714bd2b68cb4eae3d5
Ata Safapour <safapour.ata@gmail.com>
2023-07-04 17:12:41 +0330

    build: editprofile url added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProfileView, LoginView, LogoutView, RegisterView
+from .views import ProfileView, LoginView, LogoutView, RegisterView, EditProfileView
 
 app_name = 'accounts'
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", RegisterView.as_view(), name="register"),
+    path("edit-profile/", EditProfileView.as_view(), name="edit_profile")
 ]
