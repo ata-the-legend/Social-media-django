25da5f85d35c7ea6a1735b244a753f0a12b62108
Ata Safapour <safapour.ata@gmail.com>
2023-07-07 23:59:49 +0330

    feat: SoftManager added to base model

--- core/models.py
+++ core/models.py
@@ -4,13 +4,24 @@ from django.utils.translation import gettext as _
 # Create your models here.
 
 
+class SoftQuerySet(models.QuerySet):
+    def archive(self):
+        return self.update(is_active= False)
+    
+class SoftManager(models.Manager):
+    def get_queryset(self):
+        return SoftQuerySet(self.model, self._db).filter(is_active = True)
+
+
+
 class BaseModel(models.Model):
 
+    objects = SoftManager()
     class meta:
         abstract = True
         
     # id = models.UUIDField(editable=False, primary_key=True, default=uuid4)
-    is_active = models.BooleanField(_("Is_Active"), default= True)
+    is_active = models.BooleanField(_("Is_Active"), default= True, editable=False)
     create_at = models.DateTimeField(_("Create Date"), auto_now=False, auto_now_add=True) # do with mixin
     update_at = models.DateTimeField(_("Update Date"), auto_now=True, auto_now_add=False) # with mixin
 
@@ -21,3 +32,8 @@ class BaseModel(models.Model):
     def restore(self):
         self.is_active= True
         self.save()
+
+
+# class RecycleModel(BaseModel):
+#     class Meta:
+#         proxy = True
