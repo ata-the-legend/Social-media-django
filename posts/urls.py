f3a2248ea0602ad1a613555c13590ccd4264317b
Ata Safapour <safapour.ata@gmail.com>
2023-07-07 04:16:09 +0330

    build: added edit post url

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import PostListView, PostLikeView, PostCommentView, PostCreateView
+from .views import PostListView, PostLikeView, PostCommentView, PostCreateView, \
+                PostEditView, PostArchiveView
 
 app_name = 'posts'
 urlpatterns = [
     path("all/", PostListView.as_view(), name="post_list"),
     path('like/<int:post_id>', PostLikeView.as_view(), name='like'), 
     path("comments/<int:post_id>/<str:on>", PostCommentView.as_view(), name="comment"),
-    path("new-post/", PostCreateView.as_view(), name="new_post")
+    path("new-post/", PostCreateView.as_view(), name="new_post"),
+    path("edit/<int:post_id>", PostEditView.as_view(), name="post_edit"),
 ]
 
